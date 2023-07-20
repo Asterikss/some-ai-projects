10a22d4b7677b5ca15f3a139815808e742d13be4
Andre <AndreAndreM@proton.me>
2023-07-20 15:18:09 +0200

    changed the way car is rendered

--- ai_car/car.js
+++ ai_car/car.js
@@ -11,14 +11,66 @@ class Car{
         this.friction = 0.05;
         this.angle = 0;
 
+        this.damaged = false;
+
         this.sensor = new Sensor(this);
         this.controls = new Controls();
+
 
     }
 
     update(roadBorders){
         this.#move();
+        this.polygon = this.#create_polygon();
+        this.damaged = this.#assess_damage(roadBorders);
         this.sensor.update(roadBorders);
+    }
+
+    #assess_damage(roadBorders){
+        roadBorders.forEach(boarder => {
+            if(poly_intersecting(this.polygon, boarder)){
+                return true;
+            }
+        });
+        return false;
+    }
+
+    #create_polygon(){
+        const points = [];
+        const half_radious = Math.hypot(this.width, this.height) / 2;
+
+        // console.log("half_radious " + half_radious);
+        const sin_alfa = this.width / (half_radious * 2);
+        // console.log("sin_alfa " + sin_alfa);
+        const angle_rad = Math.asin(sin_alfa);
+            // const alpha = Math.atan2(this.width, this.height);
+        // console.log(angle_rad);
+        // console.log(alpha);
+    
+        // top right
+        points.push({
+            x: this.x - Math.sin(this.angle - angle_rad) * half_radious,
+            y: this.y - Math.cos(this.angle - angle_rad) * half_radious
+        })
+        // top left
+        points.push({
+            x: this.x - Math.sin(this.angle + angle_rad) * half_radious,
+            y: this.y - Math.cos(this.angle + angle_rad) * half_radious
+        })
+        // bottom left
+        points.push({
+            x: this.x - Math.sin(Math.PI + this.angle - angle_rad) * half_radious,
+            y: this.y - Math.cos(Math.PI + this.angle - angle_rad) * half_radious
+        })
+        // bottom right
+        points.push({
+            x: this.x - Math.sin(Math.PI + this.angle + angle_rad) * half_radious,
+            y: this.y - Math.cos(Math.PI + this.angle + angle_rad) * half_radious
+        })
+
+        return points;
+
+
     }
 
     #move(){
@@ -63,7 +115,7 @@ class Car{
         this.y -= Math.cos(this.angle)*this.speed;
     }
 
-    draw(ctx){
+    draw2(ctx){
         ctx.save();
         // translates the canvas origin point (0, 0) to the coordinates
         // moves the drawing position
@@ -82,5 +134,17 @@ class Car{
         ctx.restore();
 
         this.sensor.draw(ctx);
+    }
+
+    draw(ctx){
+        ctx.beginPath();
+        ctx.moveTo(this.polygon[0].x, this.polygon[0].y);
+        for (let i = 1; i < this.polygon.length; i++) {
+            ctx.lineTo(this.polygon[i].x, this.polygon[i].y);
+            
+        }
+        ctx.fill();
+        this.sensor.draw(ctx);
+
     }
 }
