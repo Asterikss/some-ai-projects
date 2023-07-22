d7172a4b97c6b0ed7d4bfe0bbcbe56be6d52f230
Andre <AndreAndreM@proton.me>
2023-07-22 12:14:52 +0200

    impl NeuralNetwork

--- ai_car/network.js
+++ ai_car/network.js
@@ -1,3 +1,22 @@
+class NauralNetwork{
+    construcotr(neuron_counts){
+        this.layers = [];
+        for (let i = 0; i < neuron_counts.length - 1; i++) {
+            this.layers.push(new Layer(neuron_counts[i], neuron_counts[i + 1]));
+        }
+    }
+
+    static feed_forward(given_inputs, network){
+        let outputs = Layer.feed_forward(given_inputs, network.layers[0]);
+        for (let i = 1; i < network.layers.length; i++) {
+            outputs = Layer.feed_forward(outputs, network.layers[1]);
+            
+        }
+
+        return outputs;
+    }
+}
+
 class Layer{
     construcotr(input_count, output_count){
         this.inputs = new Array(input_count);
